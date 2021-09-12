3686e798bbf79893fa0125a0ecdd943666c84472
SADBOYZ <>
2021-09-12 15:14:32 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+
+double Cel_far(double cel){
+    double result = (cel*(1.8)) + 32; 
+    return round(result); 
+} 
+
+double Far_cel(double far){
+    double result = (far - 32) * (0.55); 
+    return round(result); 
+}
+
+int main() {
+    std::string usr_name;
+    int choice;
+    double temp;   
+
+    std::cout << "Please enter your name:\n";
+
+    std::cin >> usr_name;
+
+    std::cout << "Welcome " << usr_name << " to the Temperature Converted Application\n"; 
+    while (choice != 3){
+        std::cout << "\nPlease type 1 for Celsius to Fahrenheit conversion.\nType 2 for Fahrenheit to Celsius conversion.\nOr type 3 to exit the program\n";
+
+        std::cin >> choice; 
+        
+        if(!std::cin){
+            std::cout << "Only input integer\n"; 
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        } 
+
+        switch(choice){
+            
+            case 1:{
+
+                std::cout << "\nPlease enter your temperature in Celsius\n"; 
+                std::cin >> temp; 
+                std::cout << "\n";
+                std::cout << "Computing... \n";  
+                //Celsius to Fahrenheit Function
+                std::cout << "The temperature in Fahrenheit is " << Cel_far(temp) << std::endl;
+                break;
+            }
+            
+            case 2:{
+
+                std::cout << "Please enter your temperature in Fahrenheit\n"; 
+                std::cin >> temp; 
+                std::cout << "\n\n\n";
+                std::cout << "Computing... \n";  
+                //Fahrenheit to Celsius Function
+                std::cout << "The temperature in Fahrenheit is " << Far_cel(temp) << std::endl;
+                break;
+            }
+            case 3:{
+                std::cout << "Goodbye!\n";
+                break;  
+            }
+            default:{
+                std::cout << "That is not an option\n";
+                break;  
+            }
+        }
+    }
+}
